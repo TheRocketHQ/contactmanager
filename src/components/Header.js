bc56710447a3aada4711f11d8ac632722c668a9f
Francisco Mateus <gtaskfm@gmail.com>
2019-03-28 09:40:56 +0100

    Created Contacts state
    contacts component was created to import info to contact component

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = props => {
   const { branding } = props;
 
   return (
-    <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb3 py--0">
+    <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py--0">
       <div className="container">
         <a href="/" className="navbar-brand">
           {branding}
