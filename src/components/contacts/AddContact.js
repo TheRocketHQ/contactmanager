bc73b988a16a72d5e0f561ed25f5e8923ab6b97a
Francisco Mateus <gtaskfm@gmail.com>
2019-03-31 17:18:58 +0200

    error Checking
    check and recheck possible bug with classname import js is not loading into page
    Error Checking and Display

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -8,13 +8,28 @@ class AddContact extends Component {
   state = {
     name: '',
     email: '',
-    phone: ''
+    phone: '',
+    errors: {}
   };
 
   onSubmit = (dispatch, e) => {
     e.preventDefault();
 
     const { name, email, phone } = this.state;
+
+    //Check for Errors
+    if (name === '') {
+      this.setState({ errors: { name: 'Name is Required' } });
+      return;
+    }
+    if (email === '') {
+      this.setState({ errors: { email: 'Email is Required' } });
+      return;
+    }
+    if (phone === '') {
+      this.setState({ errors: { phone: 'Phone is Required' } });
+      return;
+    }
 
     const newContact = {
       id: uuid(),
@@ -29,14 +44,15 @@ class AddContact extends Component {
     this.setState({
       name: '',
       email: '',
-      phone: ''
+      phone: '',
+      errors: {}
     });
   };
 
   onChange = e => this.setState({ [e.target.name]: e.target.value });
 
   render() {
-    const { name, email, phone } = this.state;
+    const { name, email, phone, errors } = this.state;
 
     return (
       <Consumer>
@@ -53,6 +69,7 @@ class AddContact extends Component {
                     placeholder="Enter Name.."
                     value={name}
                     onChange={this.onchange}
+                    error={errors.name}
                   />
                   <TextInputGroup
                     label="Email"
@@ -61,6 +78,7 @@ class AddContact extends Component {
                     placeholder="Enter Email.."
                     value={email}
                     onChange={this.onchange}
+                    error={errors.email}
                   />
                   <TextInputGroup
                     label="Phone"
@@ -68,6 +86,7 @@ class AddContact extends Component {
                     placeholder="Enter Phone.."
                     value={phone}
                     onChange={this.onchange}
+                    error={errors.phone}
                   />
                 </form>
               </div>
