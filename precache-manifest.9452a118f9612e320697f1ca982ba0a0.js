35b240c5c90bc3a47547804ae9ee8a7419bb2808
Francisco Mateus <gtaskfm@gmail.com>
2019-04-02 11:24:11 +0200

    Updates

---
+++ precache-manifest.9452a118f9612e320697f1ca982ba0a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  },
+  {
+    "revision": "4659f71161f491ed3522",
+    "url": "/contactmanager/static/js/main.f77b55fa.chunk.js"
+  },
+  {
+    "revision": "b848612abce9905d4bb4",
+    "url": "/contactmanager/static/js/2.0cd5d510.chunk.js"
+  },
+  {
+    "revision": "4659f71161f491ed3522",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "b848612abce9905d4bb4",
+    "url": "/contactmanager/static/css/2.b399dcf4.chunk.css"
+  },
+  {
+    "revision": "ad735a3d31d5a062259b997e6d48fb3c",
+    "url": "/contactmanager/index.html"
+  }
+];
