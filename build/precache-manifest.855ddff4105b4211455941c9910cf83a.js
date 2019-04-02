859ffcce3fc56c51ec6dc559c4e0f837550ed42a
Francisco Mateus <gtaskfm@gmail.com>
2019-04-02 12:16:31 +0200

    Updated build 2

---
+++ build/precache-manifest.855ddff4105b4211455941c9910cf83a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "19a73ba4d373281e2c25",
+    "url": "/static/js/main.a8b00e03.chunk.js"
+  },
+  {
+    "revision": "386aa22f07ba6202683a",
+    "url": "/static/js/2.30bb8c3c.chunk.js"
+  },
+  {
+    "revision": "19a73ba4d373281e2c25",
+    "url": "/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "386aa22f07ba6202683a",
+    "url": "/static/css/2.b399dcf4.chunk.css"
+  },
+  {
+    "revision": "50607623afcd1658fb4230bd0fc7a640",
+    "url": "/index.html"
+  }
+];
